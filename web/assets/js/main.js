80f0773032503ca184a05878b65b74d234816a56
mannycarrera4 <manuel.carrera.91@gmail.com>
2015-12-24 14:01:37 -0400

    get animation for buttons

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -3,5 +3,6 @@ define(
     'jquery'
 ],
 function($) {
-    $(document).foundation();
+    $('.message-button').append('<i class="fi-arrow-right"></i>');
+    $('.email-button').append('<i class="fi-mail"></i>');
 });
